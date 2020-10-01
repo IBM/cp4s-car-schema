93ec84b0601df831fe2ba142e9bfff251b16307d
shan <70297634+zhao-shan@users.noreply.github.com>
2020-10-01 13:19:01 -0400

    sync docs from CAR repo (#8)
    Co-authored-by: iscstg <iscstg@ie.ibm.com>

--- doc/script/docSchemaHelper.js
+++ doc/script/docSchemaHelper.js
@@ -1,10 +1,16 @@
 const importSchemaMD = require('./importSchemaMDConverter');
 const graphQLSchemaMD = require('./graphQLSchemaMDConverter');
 const graphQLSchema = require('../../src/graphql/graphqlSchema');
+const importSchema = require('../../src/schema/importSchema');
 const dotSchema = require('./dotschema');
 const fs = require('fs');
 
 fs.writeFileSync('./doc/generated/assetModel.dot', dotSchema.dotSchema.toString());
 fs.writeFileSync('./doc/generated/importSchema.md', importSchemaMD.importMD);
+importSchema.schema.additionalProperties = false;
+fs.writeFileSync(
+  './doc/generated/importSchema.json',
+  JSON.stringify(importSchema.schema, null, '    ')
+);
 fs.writeFileSync('./doc/generated/graphQLSchema.md', graphQLSchemaMD.graphQLMD);
 fs.writeFileSync('./doc/generated/graphQLSchema.sdl', graphQLSchema.graphQLSchema.toString());
